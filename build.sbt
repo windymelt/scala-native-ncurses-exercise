78db973165695ca2e10f5eed1a41d79bdbf8a5a9
windymelt <windymelt@3qe.us>
2024-10-29 01:11:17 +0900

    initial commit

---
+++ build.sbt
@@ -0,0 +1,18 @@
+ThisBuild / scalaVersion := "3.5.2"
+import bindgen.interface.Binding
+
+lazy val core = (projectMatrix in file("core"))
+  .enablePlugins(BindgenPlugin)
+  .settings(
+    name := "core",
+    bindgenBindings := Seq(
+      Binding(
+        (Compile / resourceDirectory).value / "scala-native" / "ncurses.h",
+        "libncurses",
+      ),
+    ),
+    nativeConfig ~= (_.withLinkingOptions(Seq("-lncurses"))),
+  )
+  // .jvmPlatform(scalaVersions = Seq("3.5.1"))
+  // .jsPlatform(scalaVersions = Seq("3.5.1"))
+  .nativePlatform(scalaVersions = Seq("3.5.2"))
---
+++ core/src/main/scala/example/Main.scala
@@ -0,0 +1,38 @@
+package example
+
+import scala.scalanative.unsafe.{*, given}
+import libncurses.all.*
+import scala.scalanative.unsigned.UInt
+import scala.util.control.Breaks
+
+def NCURSES_MOUSE_MASK(b: Int, m: Long): CUnsignedInt =
+  UInt.valueOf((m << (((b) - 1) * 5)).toInt)
+
+inline val KEY_MOUSE = 409
+inline val OK = 0
+
+@main def main(args: String*): Unit =
+  initscr()
+  keypad(_nc_stdscr(), true)
+  mousemask(mmask_t(NCURSES_MOUSE_MASK(6, 10L)), null)
+  noecho()
+  curs_set(0)
+  mvprintw(12, 30, c"Hello World from Scala Native!")
+  val mev = stackalloc[MEVENT]()
+  var px = 0
+  Breaks.breakable {
+    while (true) {
+      val c = getch()
+      if c == 0x71 then Breaks.break
+      if c == KEY_MOUSE then
+        if getmouse(mev) == OK then
+          clear()
+
+          px = (!mev).x;
+          if (px < 2) px = 2;
+          if (px > 77) px = 77;
+          mvprintw(23, px - 2, c"-----");
+          refresh();
+    }
+  }
+  endwin()
